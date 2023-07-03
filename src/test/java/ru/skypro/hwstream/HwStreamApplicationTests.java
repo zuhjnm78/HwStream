fd17aa1bc512ed9f031e3ab07c625dbab9a7e960
oleghenkel <oleg.henkel75@gmail.com>
2023-07-03 20:40:26 +0200

    main

---
+++ src/test/java/ru/skypro/hwstream/HwStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.hwstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
